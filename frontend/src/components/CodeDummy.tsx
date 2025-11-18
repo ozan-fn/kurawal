5f124f1276799f39e43eadb0e200400f94587908
Astha <iyandabes1@gmail.com>
2025-11-19 06:31:21 +0700

    add: all component needed for front-end

---
+++ frontend/src/components/CodeDummy.tsx
@@ -0,0 +1,38 @@
+import { Code, CodeBlock, CodeHeader } from "@/components/animate-ui/components/animate/code";
+import { FileText } from "lucide-react";
+
+interface CodeDummyProps {
+	duration: number;
+	delay: number;
+	writing: boolean;
+	cursor: boolean;
+}
+
+export const CodeDummy = ({ duration, delay, writing, cursor }: CodeDummyProps) => {
+	return (
+		<Code
+			key={`${duration}-${delay}-${writing}-${cursor}`}
+			className="p- h-[340px] w-[420px]"
+			code={`'use client';
+ 
+import * as React from "react";
+
+export interface MyComponentProps extends React.HTMLAttributes<HTMLDivElement> {
+  myProps: string;
+}
+
+export function MyComponent({ myProps, children, ...rest }: MyComponentProps) {
+  return (
+    <div {...rest}>
+      <p>{myProps}</p>
+      {children}
+    </div>
+  );
+}`}
+		>
+			<CodeHeader icon={FileText} copyButton>my-component.tsx</CodeHeader>
+
+			<CodeBlock cursor={cursor} lang="tsx" writing={writing} duration={duration} delay={delay} />
+		</Code>
+	);
+};
